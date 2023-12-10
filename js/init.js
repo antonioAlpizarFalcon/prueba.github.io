07729b62f30faa895b68755f731b477d16c31338
antonioAlpizarFalcon <152241902+antonioAlpizarFalcon@users.noreply.github.com>
2023-12-09 19:21:54 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,10 +3,10 @@
 // @ts-ignore
 firebase.initializeApp({
   apiKey: "AIzaSyDfIOlgouxMRuN9zGt2LG8OU_f_CRRTU5A",
-  authDomain: "prueba-e710b.firebaseapp.com",
-  projectId: "prueba-e710b",
-  storageBucket: "prueba-e710b.appspot.com",
-  messagingSenderId: "390643190859",
-  appId: "1:390643190859:web:178ac2bf0952530f9f91ce",
-  measurementId: "G-NQCLT1M286"
+    authDomain: "prueba-e710b.firebaseapp.com",
+    projectId: "prueba-e710b",
+    storageBucket: "prueba-e710b.appspot.com",
+    messagingSenderId: "390643190859",
+    appId: "1:390643190859:web:178ac2bf0952530f9f91ce",
+    measurementId: "G-NQCLT1M286"
     });
